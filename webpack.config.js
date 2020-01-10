7c02d3e216e455253edc5069246da7f6bc7da1e6
David Obodo <obododavid5@gmail.com>
2020-01-11 00:17:04 +0100

    -see the effect of devtool property in webpack config -devtool property was commented out after effect was seen

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 module.exports = {
     mode: "development",
     entry: "./src/myEntry.js",
+    // devtool: "none",
     output: {
         filename: "afterBuilding.js",
         path: path.resolve(__dirname, "myBuildFolder")
