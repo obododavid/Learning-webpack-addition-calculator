c7ed902815205938adc0bd995a5f185ec1e2c9e1
David Obodo <obododavid5@gmail.com>
2020-01-11 20:44:52 +0100

    -install htmlwebpackplaugin package for building html file(this enables js file name in html to be dynamic) -tell our html plgin the template it should use

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
@@ -8,6 +9,11 @@ module.exports = {
         filename: "afterBuilding.[contentHash].js",
         path: path.resolve(__dirname, "myBuildFolder")
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html"
+        })
+    ],
     module: {
         rules: [
             // {
