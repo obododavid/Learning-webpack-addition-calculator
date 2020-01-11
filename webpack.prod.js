3b9a984953dbff51e920819c2cbfe453a07778fc
David Obodo <obododavid5@gmail.com>
2020-01-12 00:00:20 +0100

    incorporate clean-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "production",
     // devtool: "none",
+    plugins: [new CleanWebpackPlugin()],
     output: {
         filename: "afterBuilding.[contentHash].js",
         path: path.resolve(__dirname, "myBuildFolder")
